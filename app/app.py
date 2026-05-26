9a0494aa774c1f8608ed3b2e2fa110de678c85f1
Amohr20 <amohrs@miumg.edu.gt>
2026-05-25 18:33:34 -0600

    Test CI/CD

--- app/app.py
+++ app/app.py
@@ -192,7 +192,7 @@ HTML = """
     </section>
 
     <div class="footer">
-    Angel Mohr - Laboratorio profesional Cloud/DevOps con infraestructura como código y despliegue automatizable.
+    Laboratorio profesional Cloud/DevOps con infraestructura como código y despliegue automatizable.
     </div>
   </main>
 </body>
